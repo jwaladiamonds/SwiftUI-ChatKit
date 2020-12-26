ff6776c5aba1f758b8b0870cee6a2035a7ebaee7
Nikhil John <nikhiljohn1010@gmail.com>
2020-12-26 14:58:31 +0530

    Added ChatKit example

---
+++ examples/ExampleChatKit/ExampleChatKit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExampleChatKit
+//
+//  Created by Nikhil John on 26/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ examples/ExampleChatKit/ExampleChatKit/ExampleChatKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleChatKitApp.swift
+//  ExampleChatKit
+//
+//  Created by Nikhil John on 26/12/20.
+//
+
+import SwiftUI
+
+@main
+struct ExampleChatKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
